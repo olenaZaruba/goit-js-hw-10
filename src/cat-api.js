c8526cddc578f36ac5852dae649850ee3179a447
olenaZaruba <z_len4ik@yahoo.fr>
2023-09-10 13:13:53 +0200

    mentor check

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,6 +2,8 @@ const url = 'https://api.thecatapi.com/v1';
 const api_key =
   'live_k60skZ9dAI1usn8K94EWSBkbBDHPZ9LfJ8U2EoZKSUwbAPd2mX1y9eQdFZiEyrUd';
 
+// import { refs } from './index.js';
+
 export function fetchBreeds() {
   return fetch(`${url}/breeds?api_key=${api_key}`).then(response => {
     if (!response.ok) {
--- src/index.js
+++ src/index.js
@@ -4,42 +4,49 @@ import 'slim-select/dist/slimselect.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './styles.css';
 
-const refs = {
+export const refs = {
   selector: document.querySelector('.breed-select'),
   divCatInfo: document.querySelector('.cat-info'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
 };
-refs.loader.classList.replace('loader', 'is-hidden');
-refs.error.classList.add('is-hidden');
-refs.divCatInfo.classList.add('is-hidden');
 
-let breedsId = [];
-fetchBreeds()
-  .then(data => {
-    data.forEach(element => {
-      breedsId.push({ text: element.name, value: element.id });
-    });
-    new SlimSelect({
-      select: refs.selector,
-      data: breedsId,
-    });
-  })
-  .catch(onFetchError);
+function createCatList() {
+  refs.loader.classList.remove('is-hidden');
+  refs.selector.classList.add('is-hidden');
+  refs.error.classList.add('is-hidden');
+
+  fetchBreeds()
+    .then(data => {
+      const optionsList = data
+        .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+        .join(' ');
+
+      refs.selector.innerHTML = optionsList;
+
+      new SlimSelect({
+        select: refs.selector,
+      });
+      refs.loader.classList.add('is-hidden');
+      refs.selector.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+}
+createCatList();
 
 refs.selector.addEventListener('change', onChangeSelect);
 
 function onChangeSelect(event) {
-  refs.loader.classList.replace('is-hidden', 'loader');
-
-  refs.selector.classList.add('is-hidden');
-
+  refs.loader.classList.remove('is-hidden');
+  //   refs.selector.classList.remove('is-hidden');
+  refs.divCatInfo.classList.add('is-hidden');
   const oneBreedId = event.currentTarget.value;
+
   fetchCatByBreed(oneBreedId)
     .then(data => {
-      refs.loader.classList.replace('loader', 'is-hidden');
-      refs.selector.classList.remove('is-hidden');
-
+      //   refs.loader.classList.add('is-hidden');
+      //   refs.selector.classList.remove('is-hidden');
+      //   refs.divCatInfo.classList.remove('is-hidden');
       const { url, breeds } = data[0];
       console.log(data[0]);
 
@@ -51,7 +58,8 @@ function onChangeSelect(event) {
                     <h1>${breeds[0].name}</h1>
                     <p>${breeds[0].description}</p>
                     <p><b>Temperament:</b> ${breeds[0].temperament}</p>
-                </div>`;
+              </div>`;
+      refs.loader.classList.add('is-hidden');
       refs.divCatInfo.classList.remove('is-hidden');
     })
 
@@ -60,7 +68,9 @@ function onChangeSelect(event) {
 
 function onFetchError(error) {
   refs.selector.classList.remove('is-hidden');
-  refs.loader.classList.replace('loader', 'is-hidden');
+  refs.loader.classList.add('is-hidden');
+  refs.divCatInfo.innerHTML = '';
+  //   refs.error.classList.remove('is-hidden');
 
   Notify.failure('Oops! Something went wrong! ', { timeout: 8000 });
 }
